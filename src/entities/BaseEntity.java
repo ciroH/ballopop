f723b2b23976056ba4149390e7ffb8fa1f2727d1
ciroH <63865991+ciroH@users.noreply.github.com>
2022-04-05 18:42:34 -0300

    Added BaseEntity to entities layer

---
+++ src/entities/BaseEntity.java
@@ -0,0 +1,24 @@
+package entities;
+
+public class BaseEntity {
+	int id;
+	String password;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
