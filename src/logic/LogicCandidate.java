4b20ada62824f83b073492381562d285e705db47
matesuli <63865991+ciroH@users.noreply.github.com>
2022-05-23 17:13:13 -0300

    Added getCandidates method to LogicCandidate

--- src/logic/LogicCandidate.java
+++ src/logic/LogicCandidate.java
@@ -1,5 +1,17 @@
 package logic;
 
+import java.util.LinkedList;
+
+import data.DataCandidate;
+import entities.Candidate;
+
 public class LogicCandidate {
 
+	public LinkedList<Candidate> getCandidates(){
+	DataCandidate candidateDataAccessObject = new DataCandidate();
+	LinkedList<Candidate> candidateList = new LinkedList<>();
+	candidateList = candidateDataAccessObject.getAll();
+		return candidateList;
+	}
+	
 }
