9c9c760c2e464aef37d2413f6a78a3de5242dee7
matesuli <63865991+ciroH@users.noreply.github.com>
2022-06-17 19:44:55 -0300

    Added DataUser.markVote method

--- src/data/DataUser.java
+++ src/data/DataUser.java
@@ -42,4 +42,30 @@ public class DataUser {
 		return userObject;
 	}
 	
+	public boolean markVote(User credentials, int candidateId) {
+		DataCandidate dataCandidate = new DataCandidate();
+		boolean voteConfirmation = false;
+		String query = "update user set voted=1 where id=? and password=?";
+		PreparedStatement voteStatement = null;
+		try {
+			if(dataCandidate.increaseVotes(candidateId)) {
+			voteStatement = DbConnector.getInstance().getConn().prepareStatement(query);
+			voteStatement.executeUpdate();
+			voteConfirmation = true;
+			}
+		} catch (SQLException e) {
+			voteConfirmation = false;
+		} finally {
+			try {
+				if(voteStatement != null){
+					voteStatement.close();
+				}
+				DbConnector.getInstance().releaseConn();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return voteConfirmation;
+	}
+	
 }
