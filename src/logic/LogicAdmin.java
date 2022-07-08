3046cbf687325d58c657f238371db65a35cabd6f
matesuli <63865991+ciroH@users.noreply.github.com>
2022-07-07 23:39:24 -0300

    Added validateLogIn method to logic.LogicAdmin

--- src/logic/LogicAdmin.java
+++ src/logic/LogicAdmin.java
@@ -1,5 +1,23 @@
 package logic;
 
+import java.sql.SQLException;
+
+import data.DataAdmin;
+import entities.Admin;
+
 public class LogicAdmin {
 
+	public boolean validateLogIn(int key, String password) throws SQLException {
+		Admin credentials = new  Admin(key, password);
+		DataAdmin data = new DataAdmin();
+		boolean logInConfirmation = false;
+		try {
+			logInConfirmation = data.logIn(credentials);
+		} catch (SQLException e) {
+			throw e;
+		}
+		
+		return logInConfirmation;
+	}
+	
 }
