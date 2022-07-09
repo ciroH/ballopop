4f441a54b4f5fd10a7c3cd04ee916bdbc38958c7
matesuli <63865991+ciroH@users.noreply.github.com>
2022-07-09 20:57:10 -0300

    Added servlets/ManageMenu to controller layer

---
+++ src/servlets/ManageMenu.java
@@ -0,0 +1,55 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({ "/ManageMenu", "/managemenu" })
+public class ManageMenu extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ManageMenu() {
+        super();
+    }
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getAttribute("sessionID").toString()!=null) {
+			doPost(request, response);
+		} else {
+			request.getSession().invalidate();
+			forwardToIndex(request, response);
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		switch (request.getParameter("destination")) {
+		case "menu":
+			request.getRequestDispatcher("WEB-INF/mainPanel.jsp").forward(request, response);
+			break;
+		case "candidates":
+			request.getRequestDispatcher("WEB-INF/candidatesPanel.jsp").forward(request, response);
+			break;
+		case "users":
+			request.getRequestDispatcher("WEB-INF/usersPanel.jsp").forward(request, response);
+			break;
+		case "stats":
+			
+			break;
+
+		default:
+			forwardToIndex(request, response);
+			break;
+		}
+	}
+	private void forwardToIndex(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+}
