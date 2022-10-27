e162cdce58ae0a58f48c1e7caf215d89764166e7
matesuli <63865991+ciroH@users.noreply.github.com>
2022-10-27 16:43:16 -0300

    Modified DataStats.getVotes() return type: HashMap to ArrayList

--- src/data/DataStats.java
+++ src/data/DataStats.java
@@ -2,19 +2,14 @@ package data;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import entities.Candidate;
 
 public class DataStats {
 
-	public HashMap<String, Integer> getVotes() throws SQLException{
+	public ArrayList<Candidate> getVotes() throws SQLException{
 		DataCandidate data = new DataCandidate();
-		HashMap<String, Integer> votesCount = new HashMap<>();
-		ArrayList<Candidate> candidates = new ArrayList<>(data.getAll());
-		for (Candidate candidate : candidates) {
-			votesCount.put(candidate.getName(), candidate.getVotes());		
-		}
+		ArrayList<Candidate> votesCount = new ArrayList<>(data.getAll());
 		return votesCount;
 	}
 	
